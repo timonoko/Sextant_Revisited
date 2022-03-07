bae6a099e4a50cd4a75db3f51d2a46263b34040b
Timo Noko <timonoko@users.noreply.github.com>
2022-03-07 11:02:50 +0200

    --

--- sun.py
+++ sun.py
@@ -3,7 +3,7 @@ from astropy.time import Time
 import astropy.units as u
 
 def alt(mon,day,hour,mins,lat=60.2,lon=25):
-    loc = coord.EarthLocation(lon=25 * u.deg, lat=lat * u.deg)
+    loc = coord.EarthLocation(lon=lon * u.deg, lat=lat * u.deg)
     times = f'2022-{mon}-{day}T{hour}:{mins}:00'
     now  = Time(times, format='isot', scale='utc')
     altaz = coord.AltAz(location=loc, obstime=now)
